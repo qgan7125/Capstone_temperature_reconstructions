5aa290cfd9379f79501f343719173191a09cc616
qgan7125 <qgan@email.arizona.edu>
2022-04-23 14:38:25 -0700

    updated results with new data

--- Global.R
+++ Global.R
@@ -1,5 +1,4 @@
 # load libraries
-library(dplyr)
 library(ggplot2)
 library(boot)
 library(knitr)
@@ -15,11 +14,16 @@ library(MASS)
 library(quantreg)
 library(gmodels)
 library(greekLetters)
+library(ggthemes)
+library(dplyr)
 
 # load data in
 data_low <- read.csv("./Data/Dataset_S1_Mar8.csv")
 data_intermediate <- read.csv("./Data/Dataset_S2_Apr20_V2.csv")
 data_high <- read.csv("./Data/Dataset_S3_Apr20_V2.csv")
+data_low$T2 <- data_low$Temperature
+data_intermediate$T2 <- data_intermediate$Temperature
+data_high$T2 <- data_high$Temperature
 
 
 # functions from BayClump
